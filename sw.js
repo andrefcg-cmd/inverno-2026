42d3a4c2bfcc382136b0aef923d54eca0b12913a
andrefcg-cmd <andrefcg@gmail.com>
2026-02-04 22:56:56 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,32 +1,5 @@
-const CACHE_NAME = 'isapaes-v5';
-const BASE = '/inverno-2026/';
-const urlsToCache = [
-    BASE,
-    BASE + 'index.html',
-    BASE + 'icon-192.png',
-    BASE + 'icon-512.png'
-];
+const V = 'v6';
 
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-    e.waitUntil(
-        caches.keys().then(keys => Promise.all(
-            keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-        ))
-    );
-    self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-    e.respondWith(
-        fetch(e.request).then(r => {
-            const clone = r.clone();
-            caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
-            return r;
-        }).catch(() => caches.match(e.request))
-    );
-});
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
